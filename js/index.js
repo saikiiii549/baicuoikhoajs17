adbdb9a60b47544a41ee8a4dec0e947d768253cc
saikiiii549 <koten1234z@gmail.com>
2023-06-15 19:25:00 +0700

    tt

---
+++ js/index.js
@@ -0,0 +1,37 @@
+import Login from "./login.js"
+import Register from "./register.js"
+
+
+class App {
+    activeScreen
+    container
+ 
+    constructor(container){
+        this.container = container
+    }
+
+    changeActiveScreen(screen) {
+        if (this.activeScreen !== undefined){
+            this.container.innerHTML = ""
+        }
+
+        this.activeScreen = screen
+        this.activeScreen.initRender(this.container)
+    }
+}
+
+const container = document.getElementById("app")
+
+const register = new Register()
+const login = new Login()
+
+
+
+
+
+const app = new App(container)
+app.changeActiveScreen(login)
+
+
+//export instant của app chứ ko export class vì App là duy nhất 
+export default app;
